d5bf8b40f1b6b4ea407806ac8f7755617bf42218
Mr.w <122627018@qq.com>
2016-11-30 12:03:22 +0800

    2016.11.30 12.03

--- app/src/main/java/com/wxxiaomi/ming/bicyclewebmodule/ConstantValue.java
+++ app/src/main/java/com/wxxiaomi/ming/bicyclewebmodule/ConstantValue.java
@@ -4,8 +4,8 @@ package com.wxxiaomi.ming.bicyclewebmodule;
  * Created by 12262 on 2016/11/21.
  */
 public class ConstantValue {
-    public static String SERVER_URL = "http://192.168.1.103:8080/ElectricBicycleServer/";
-    public static String OSS_MY_URL = "http://192.168.1.103:8080/OssTokenGetServer/Oss";
+    public static String SERVER_URL = "http://192.168.1.46:8080/ElectricBicycleServer/";
+    public static String OSS_MY_URL = "http://192.168.1.46:8080/OssTokenGetServer/Oss";
     public static boolean isWebCacheOpen = false;
 
     private double lat = 23.566701;
